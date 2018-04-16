4dc943641c191c5f59c12fabce811ac6c9184e41
Taylor Stemple <taylorstemple@gmail.com>
2018-04-15 22:18:27 -0700

    reshuffling stuff

--- lab-taylor/src/components/BaseInputText.vue
+++ lab-taylor/src/components/BaseInputText.vue
@@ -18,9 +18,9 @@ export default {
   computed: {
     listeners () {
       return {
-        // Pass all component listeners directly to input
+       
         ...this.$listeners,
-        // Override input listener to work with v-model
+       
         input: event => this.$emit('input', event.target.value)
       }
     }
--- lab-taylor/src/index.js
+++ lab-taylor/src/index.js
@@ -3,9 +3,9 @@ import App from './App.vue';
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
 })
+
